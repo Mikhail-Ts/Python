3ba1aaca6b9e85a14ef4ad338b45378e538259e4
Mikhail <tsybulkomk@yandex.ru>
2022-11-18 11:12:43 +0300

    Update Task_3 Seminar_4

--- Seminar_4/Task_3.py
+++ Seminar_4/Task_3.py
@@ -1,2 +1,9 @@
 # Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+def UniqList (list):
+    list = [i for i in list if list.count(i) == 1]
+    return list
+
+list = list(input("Введите числа через пробел: "))
+print (UniqList(list))
