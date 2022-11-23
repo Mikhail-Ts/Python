e922553c0d62a8cc8be9985c9bc71bcc20f26dd2
Mikhail <tsybulkomk@yandex.ru>
2022-11-23 17:17:26 +0300

    Update Task_2 Seminar_5 selection_mode

--- Seminar_5/Task_2.py
+++ Seminar_5/Task_2.py
@@ -58,13 +58,11 @@ def player_vs_player():
     if count == 0:
         print(f'{player_1} ПОБЕДИЛ')
 
-# player_vs_player()
-
 def player_vs_bot():
     sweet_total = 2021
     max_take = 28
     count = 0
-    player_1 = input('\nВведите имя 1 игрока: ')
+    player_1 = input('\nВведите имя игрока: ')
     player_2 = "bot"
 
     print('\nДля начала опеределим кто первый начнет игру.\n')
@@ -118,8 +116,18 @@ def player_vs_bot():
             print('Игра окончена')
 
     if count == 1:
-        print(f'{player_2} ПОБЕДИЛ')
+        print(f'Победил: {player_2}')
     if count == 0:
-        print(f'{player_1} ПОБЕДИЛ')
+        print(f'Победил: {player_1}')
 
-player_vs_bot()
+def selection_mode():
+    mode = int(input(f'Выберите режим:\n1 - player_vs_player\n2 - player_vs_bot\n'))
+    if mode == 1:
+        player_vs_player()
+    elif mode == 2:
+        player_vs_bot()
+    else:
+        print(f'Введите число 1 или 2:')
+        selection_mode()
+
+selection_mode()
