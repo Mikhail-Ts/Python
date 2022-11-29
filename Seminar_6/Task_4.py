c47dea9b4793f9158be308ce1a92e94da43608e4
Mikhail <tsybulkomk@yandex.ru>
2022-11-29 18:15:39 +0300

    Create Seminar_6 Task3 and Task4

---
+++ Seminar_6/Task_4.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. 
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+n = list(map(int, input("Введите числа через пробел: ").split()))
+# программа до улучшения
+
+# def sumpairs(n):
+#     if len(n)%2 == 0:
+#         l = len(n)//2
+#     else:
+#         l = len(n)//2+1
+#     new_list = [n[i]*n[len(n)-i-1] for i in range(l)]
+#     print(new_list)
+# sumpairs(n)
+
+# Программа после улучшения
+import math
+print(list([a*b for a,b in zip(n[0:math.ceil(len(n)/2)],n[::-1])]))
